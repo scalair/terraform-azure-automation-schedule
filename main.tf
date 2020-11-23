e2a032ae60e6feb19f12c0c79cf09bfed0e18388
xat <descamps.jeremy@gmail.com>
2020-11-23 16:08:19 +0100

    remove: obsolete provider block

--- main.tf
+++ main.tf
@@ -1,7 +1,3 @@
-provider "azurerm" {
-  features {}
-}
-
 resource "azurerm_automation_schedule" "schedule" {
   name                    = var.name
   resource_group_name     = var.resource_group_name
